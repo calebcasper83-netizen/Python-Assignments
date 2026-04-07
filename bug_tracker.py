a15065f9ebc27fc7ee32855270a6f23297c009df
calebcasper83-netizen <calebcasper83@gmail.com>
2026-04-06 21:59:35 -0500

    Add files via upload

---
+++ bug_tracker.py
@@ -0,0 +1,38 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT 11A REVISED: THE BUG TRACKING LOG
+-----------------------------------------------------------------------
+[ ] 1. Program uses a while loop to keep asking for bugs.
+[ ] 2. Uses the datetime module to get a timestamp format.
+[ ] 3. Stores the timestamp, file name, description, and priority in a dictionary.
+[ ] 4. Uses `with open("bug_log.txt", "a")` to append to the file safely.
+[ ] 5. The bug_log.txt file is formatted neatly with newlines.
+Name: Caleb Casper
+-----------------------------------------------------------------------
+"""
+
+import datetime
+
+while True:
+    choice = input("Enter 'log' to record a bug, or 'quit' to stop: ").strip().lower()
+
+    if choice == "quit":
+        print("Bug log successfully updated.")
+        break
+
+    elif choice == "log":
+        file_name = input("File name: ").strip()
+        description = input("Description of error: ").strip()
+        priority = input("Priority (High/Medium/Low): ").strip()
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        bug = {timestamp: [file_name, description, priority]}
+
+        with open("bug_log.txt", "a") as file:
+            file.write(f"[{timestamp}]\n")
+            file.write(f"File: {file_name}\n")
+            file.write(f"Status: {description}\n")
+            file.write(f"Priority: {priority}\n")
+    else:
+        print("Invalid reponse. Please type 'log' or 'quit'.")
